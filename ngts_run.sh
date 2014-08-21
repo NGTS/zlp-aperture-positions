24bc4843c24c9e50f7ecb9bb1efe7628bb6f5235
Simon Walker <s.r.walker101@googlemail.com>
2014-08-21 16:13:46 +0100

    Passing help flag straight through to main script

--- ngts_run.sh
+++ ngts_run.sh
@@ -7,7 +7,11 @@ main() {
     shift
     local readonly args=$@
 
-    python ${PWD}/visualise_apertures.py ${dir} -p ${dir} ${args}
+    if [[ "${dir}" == "-h" || "${dir}" == "--help" ]]; then
+        python ${PWD}/visualise_apertures.py -h
+    else
+        python ${PWD}/visualise_apertures.py ${dir} -p ${dir} ${args}
+    fi
 }
 
 main "$@"
